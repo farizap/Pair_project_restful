54d7561bea555890e9c97d305c60796628a1e0a5
fikriamri <famri@alterra.id>
2019-08-01 22:20:53 +0700

    add event resources

--- rest_svc/blueprints/__init__.py
+++ rest_svc/blueprints/__init__.py
@@ -58,7 +58,8 @@ def non_internal_required(fn):
 ####Database####
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://alta123:h@localhost:3306/pair_project'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://alta123:h@localhost:3306/pair_project'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://fikriamri:threecheers@localhost:3306/pair_project'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -90,21 +91,25 @@ def after_request(response):
 from blueprints.client.resources import bp_client
 # from blueprints.user.resources import bp_user
 # from blueprints.book.resources import bp_books
-# from blueprints.auth import bp_auth
+from blueprints.auth import bp_auth
 # from blueprints.weather.resources import bp_weather
 # from blueprints.rent.resources import bp_rent
 from blueprints.hqpredict.resources import bp_hqpredict
 from blueprints.trip.resources import bp_trips
+from blueprints.airport.resources import bp_airport
+from blueprints.event.resources import bp_event
 
 
 # app.register_blueprint(bp_person, url_prefix='/person')
 app.register_blueprint(bp_client, url_prefix='/client' )
 # app.register_blueprint(bp_books, url_prefix='/books' )
 # app.register_blueprint(bp_user, url_prefix='/user' )
-# app.register_blueprint(bp_auth, url_prefix='/token')
+app.register_blueprint(bp_auth, url_prefix='/token')
 # app.register_blueprint(bp_weather, url_prefix='/weather')
 # app.register_blueprint(bp_rent, url_prefix='/rent')
 app.register_blueprint(bp_hqpredict, url_prefix='/predict')
 app.register_blueprint(bp_trips, url_prefix='/trip')
+app.register_blueprint(bp_airport, url_prefix='/airport')
+app.register_blueprint(bp_event, url_prefix='')
 
 db.create_all()
---
+++ rest_svc/blueprints/event/resources.py
@@ -0,0 +1,125 @@
+from flask import Blueprint
+from flask_restful import Resource, Api, reqparse, marshal, inputs
+
+from .model import Events
+from blueprints import db, app
+from sqlalchemy import desc
+
+from blueprints import internal_required, non_internal_required
+from flask_jwt_extended import jwt_required, get_jwt_claims
+
+from blueprints.hqpredict.resources import UserGetHQPredict
+from blueprints.trip.resources import Trips
+
+
+# 'client' penamaan (boleh diganti)
+bp_event = Blueprint('event', __name__)
+api = Api(bp_event)
+
+class EventUserResource(Resource):
+    host = "https://api.predicthq.com/v1/events/"
+    token = "e6KsG50OLWLBog4ZyTXTpu1OMKCUgS"
+    
+    @jwt_required
+    @non_internal_required
+    def post(self):
+
+        raw = UserGetHQPredict().get()
+
+        # return raw
+
+        title = raw[0]['results'][0]['title']
+        event_id = raw[0]['results'][0]['id']
+        address = raw[0]['results'][0]['entities'][0]["formatted_address"]
+        venue = raw[0]['results'][0]['entities'][0]["name"]
+        start_time = raw[0]['results'][0]['start']
+        end_time = raw[0]['results'][0]['end']
+
+        event = Events.query.filter_by(event_id='event_id')
+        claim = get_jwt_claims()
+        # if event is None:
+        event = Events(title, event_id, address, venue, start_time, end_time)
+        db.session.add(event)
+        db.session.commit()
+
+            # event = marshal(event, Events.response_field)
+
+            # trip = Trips(claim['id'], marshal(event, Events.response_field)['id'])
+            # db.session.add(trip)
+            # db.session.commit()
+            # return event
+        #     db.session.expire_all()
+
+        # trip = Trips(claim['id'], event['id'])
+        # db.session.add(trip)
+        # db.session.commit()
+
+        return marshal(event, Events.response_field), 200, {'Content-Type':'application/json'}
+    
+    # @jwt_required
+    # @internal_required
+    def put(self,id):
+        parser = reqparse.RequestParser()
+        parser.add_argument('client_key', location='json', required=True)
+        parser.add_argument('client_secret', location='json', required=True)
+        parser.add_argument('status',type=inputs.boolean, location='json')
+        args = parser.parse_args()
+
+        qry = Clients.query.get(id)
+
+        if qry is None:
+            return {'status':'NOT_FOUND'}, 404
+
+        qry.client_key = args['client_key']
+        qry.client_secret = args['client_secret']
+        qry.status = args['status']
+        db.session.commit()
+        return marshal(qry, Clients.response_field), 200, {'Content-Type':'application/json'}
+
+    # @jwt_required
+    # @internal_required
+    def delete(self,id):
+        qry = Clients.query.get(id)
+        if qry is None:
+            return {'status':'NOT_FOUND'}, 404
+
+        db.session.delete(qry)
+        db.session.commit()
+
+        return {'status':'DELETED'}, 200
+
+# class ClientList(Resource):
+
+#     def __init__(self):
+#         pass
+
+#     # @jwt_required
+#     # @internal_required
+#     def get(self):
+#         parser = reqparse.RequestParser()
+#         parser.add_argument('p', type=int, location='args', default=1)
+#         parser.add_argument('rp',type=int, location='args', default=25)
+#         parser.add_argument('filterbyclientid',location='args', help='invalid client_id',type=int)
+#         parser.add_argument('filterbystatus',location='args', type=inputs.boolean, help='invalid status', choices=(True,False))
+
+#         args =parser.parse_args()
+
+#         offset = (args['p'] * args['rp']) - args['rp']
+
+#         qry = Clients.query
+
+#         if args['filterbystatus'] is not None:
+#             qry = qry.filter_by(status=args['filterbystatus'])
+
+#         if args['filterbyclientid'] is not None:
+#             qry = qry.filter_by(id=args['filterbyclientid'])
+
+#         result = []
+#         for row in qry.limit(args['rp']).offset(offset).all():
+#             result.append(marshal(row,Clients.response_field))
+        
+#         return result, 200, {'Content-Type':'application/json'}
+
+
+api.add_resource(EventUserResource, '/user/event',)
+# api.add_resource(ClientList,'','/list')
